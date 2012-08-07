3d620c8fa723739dcff585de3e4ae62d05be4bc5
ccpowell <chris_powell@drcog.org>
2012-08-07 10:04:33 -0600

    updated schema

--- OmbMvc4/OmbMf/Models/Facility.cs
+++ OmbMvc4/OmbMf/Models/Facility.cs
@@ -12,9 +12,25 @@ namespace OmbMf.Models
 
         [Required]
         public string Name { get; set; }
+
+        [Required]
         public int FacilityTypeId { get; set; }
         public FacilityType FacilityType { get; set; }
+
         public int? OmbudsmanId { get; set; }
         public Ombudsman Ombudsman { get; set; }
+
+        public string Address1 { get; set; }
+        public string Address2{ get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+        public bool IsActive { get; set; }
+        public int? NumberOfBeds { get; set; }
+        public bool? IsMedicaid { get; set; }
+        public bool? IsContinuum { get; set; }
+
     }
 }
