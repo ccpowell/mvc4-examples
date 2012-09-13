ab29fb0af776f4400dca0e5711cb7ad2c4101e9c
ccpowell <chris_powell@drcog.org>
2012-09-13 17:50:27 -0600

    tweaked

--- OmbMvc4/OmbMf/Models/Ombudsman.cs
+++ OmbMvc4/OmbMf/Models/Ombudsman.cs
@@ -16,6 +16,14 @@ namespace OmbMf.Models
         [Required]
         public string UserName { get; set; }
 
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+
         // this would lead to circular references. 
         // although not impossible to handle, it is simpler not to.
         //public List<Facility> Facilities { get; set; }
