26b01ce3691bad3f1a7de1c4017817076346b2f6
ccpowell <chris_powell@drcog.org>
2012-09-19 17:32:22 -0600

    use installed datatable styles

--- Ombudsman/Ombudsman.Site/App_Start/BundleConfig.cs
+++ Ombudsman/Ombudsman.Site/App_Start/BundleConfig.cs
@@ -32,6 +32,8 @@ namespace Ombudsman.Site
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
 
+            bundles.Add(new StyleBundle("~/Content/datatable").Include("~/Content/DataTables/css/jquery.dataTables.css"));
+
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
                         "~/Content/themes/base/jquery.ui.resizable.css",
