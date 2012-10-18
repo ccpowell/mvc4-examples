93b72d9afdc6c4997493e9e718f95ffc9710e090
ccpowell <chris_powell@drcog.org>
2012-10-18 11:19:40 -0600

    added QA configuration

--- Ombudsman/Ombudsman.Site/Controllers/HomeController.cs
+++ Ombudsman/Ombudsman.Site/Controllers/HomeController.cs
@@ -19,18 +19,7 @@ namespace Ombudsman.Site.Controllers
             var repo = new OmbudsmanDb.OmbudsmanRepository();
             var facilities = repo.GetFacilityTypes();
             ViewBag.FacilityTypeId = new SelectList(facilities, "FacilityTypeId", "Name");
-
-            //var ombudsmen = repo.GetOmbudsmen();
-            //ViewBag.OmbudsmanId = new SelectList(ombudsmen, "OmbudsmanId", "Name");
-
             ViewBag.IsManager = System.Web.Security.Roles.IsUserInRole("Manager");
-            ViewBag.IsNewSession = "Session is new? " + Session.IsNewSession;
-            Response.SetCookie(new HttpCookie("hoover", Session.SessionID));
-            Session["something"] = 42;
-
-            Logger.Debug("Session is new = " + Session.IsNewSession);
-            Logger.Debug("Session ID = " + Session.SessionID);
-            Logger.Debug("Session cookie mode = " + Session.CookieMode);
 
             return View();
         }
